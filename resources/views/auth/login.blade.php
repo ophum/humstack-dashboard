8b80b1d0a3980849a20e16cf509d717dc6f30eaa
Takahiro Inagaki <inagaki0106@gmail.com>
2020-11-11 13:10:04 +0900

    fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => 'login', 'title' => __('Material Dashboard')])
+@extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => 'login', 'titlePage' => __('Material Dashboard')])
 
 @section('content')
 <div class="container" style="height: auto;">
