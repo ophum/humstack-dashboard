fb0fecf6970898871929179165a51f5a2ff35ce2
ophum <inagaki0106@gmail.com>
2021-01-17 23:47:01 +0900

    [update] footerを更新

--- resources/views/layouts/footers/auth.blade.php
+++ resources/views/layouts/footers/auth.blade.php
@@ -1,35 +1,14 @@
 <footer class="footer">
   <div class="container-fluid">
-    <nav class="float-left">
-      <ul>
-        <li>
-          <a href="https://www.creative-tim.com">
-              {{ __('Creative Tim') }}
-          </a>
-        </li>
-        <li>
-          <a href="https://creative-tim.com/presentation">
-              {{ __('About Us') }}
-          </a>
-        </li>
-        <li>
-          <a href="http://blog.creative-tim.com">
-              {{ __('Blog') }}
-          </a>
-        </li>
-        <li>
-          <a href="https://www.creative-tim.com/license">
-              {{ __('Licenses') }}
-          </a>
-        </li>
-      </ul>
-    </nav>
+
     <div class="copyright float-right">
       &copy;
+      2020 -
       <script>
         document.write(new Date().getFullYear())
-      </script>, made with <i class="material-icons">favorite</i> by
-      <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a> and <a href="https://www.updivision.com" target="_blank">UPDIVISION</a> for a better web.
+      </script>, made with by <a href="https://github.com/ophum" target="_blank">hum_op</a>.<br>
+      powered by 
+      <a href="https://www.creative-tim.com/product/material-dashboard-laravel" target="_blank">Creative Tim material-dashboard-laravel</a>
     </div>
   </div>
 </footer>
