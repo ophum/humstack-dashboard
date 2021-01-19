a6c308492a237f99d26303200f7426ad2ca244e4
Takahiro Inagaki <inagaki0106@gmail.com>
2021-01-19 22:49:11 +0900

    fix model

--- app/Models/DeploySetting.php
+++ app/Models/DeploySetting.php
@@ -9,10 +9,22 @@ class DeploySetting extends Pivot
 {
     use HasFactory;
 
+    protected $table = 'deploy_settings';
+
     protected $guarded = [];
 
     public function node()
     {
         return $this->belongsTo('App\Models\Node');
+    }
+
+    public function problem()
+    {
+        return $this->belongsTo('App\Models\Problem');
+    }
+
+    public function team()
+    {
+        return $this->belongsTo('App\Models\Team');
     }
 }
